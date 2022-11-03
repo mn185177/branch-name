d239ad712de9f177b20db7f48ebe25b3fffc3d90
Manohar Nuthalapati <103932056+mn185177@users.noreply.github.com>
2022-11-03 20:19:59 +0530

    Update run-action.js

--- src/run-action.js
+++ src/run-action.js
@@ -3,9 +3,9 @@ const writeComment = require('./github/write-comment');
 const DEFAULT_COMMENT_FOR_INVALID_BRANCH_NAME = 'The name of this branch is not \n following the standards of this project!';
 const DEFAULT_COMMENT_FOR_VALID_BRANCH_NAME = 'The name of this branch is fallowing \n the standards of this project!';
 module.exports = async tools => {
-  const branchPattern = tools.inputs.branch_pattern;
+  const branchPattern = /\D+-\d+/;
   const failIfInvalidBranchName = tools.inputs.fail_if_invalid_branch_name;
-  const ignoreBranchPattern = tools.inputs.ignore_branch_pattern;
+  const ignoreBranchPattern = master|qa*|rel-*;
   const commentForInvalidBranchName = tools.inputs.comment_for_invalid_branch_name || DEFAULT_COMMENT_FOR_INVALID_BRANCH_NAME;
   const commentForValidBranchName = tools.inputs.comment_for_valid_branch_name || DEFAULT_COMMENT_FOR_VALID_BRANCH_NAME;
   const branchName = tools.context.payload.pull_request.title;
