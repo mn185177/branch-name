6822484a70564c33616b9e0df27bcb9614370a7a
Manohar Nuthalapati <103932056+mn185177@users.noreply.github.com>
2022-11-03 20:43:09 +0530

    Update run-action.js

--- src/run-action.js
+++ src/run-action.js
@@ -5,20 +5,20 @@ const DEFAULT_COMMENT_FOR_VALID_BRANCH_NAME = 'The name of this branch is fallow
 module.exports = async tools => {
   const branchPattern = /\D+-\d+/;
   const failIfInvalidBranchName = tools.inputs.fail_if_invalid_branch_name;
-  const ignoreBranchPattern = 'master|qa*|rel-*';
+  //const ignoreBranchPattern = 'master|qa*|rel-*';
   const commentForInvalidBranchName = tools.inputs.comment_for_invalid_branch_name || DEFAULT_COMMENT_FOR_INVALID_BRANCH_NAME;
   const commentForValidBranchName = tools.inputs.comment_for_valid_branch_name || DEFAULT_COMMENT_FOR_VALID_BRANCH_NAME;
   const branchName = tools.context.payload.pull_request.title;
 	tools.log.info(tools.context.payload.pull_request.title);
   const isValidBranchName = new RegExp(branchPattern).test(branchName);
 
-  if (ignoreBranchPattern) {
+/*  if (ignoreBranchPattern) {
     const isIgnoredBranch = new RegExp(ignoreBranchPattern).test(branchName);
     if (isIgnoredBranch) {
       tools.log.info('This PR should be ignored');
       return;
     }
-  }
+  }*/
 
   if (isValidBranchName) {
     tools.log.info('This PR has valid subject');
